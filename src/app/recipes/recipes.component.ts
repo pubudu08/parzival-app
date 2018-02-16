b0c06b5f06c5664ee448eb08c43b3057881e2c4a
Pubudu  Dissanayake <u5857495@anu.edu.au>
2018-02-16 22:35:33 +1100

    Recipe Model

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {Recipe} from './recipe.model';
-import {RecipeService} from './recipe.service';
+import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -12,8 +12,7 @@ import {RecipeService} from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor(private recipeService: RecipeService) {
-  }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
     this.recipeService.recipeSelected
